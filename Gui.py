515b679aa786a42d9f8b4ded68ca5a99570b78b5
WeifaGan <44939038+WeifaGan@users.noreply.github.com>
2021-05-11 15:53:31 +0800

    Add files via upload

--- Gui.py
+++ Gui.py
@@ -1,110 +1,212 @@
+import json
 from tkinter.filedialog import askdirectory
+import os 
 import tkinter as tk
 from tkinter import messagebox
 from arg import * 
+from PIL import Image, ImageTk
 import numpy as np
-
+from tkinter import messagebox
+import pandas as pd
 
 class GUI(object):
-    def __init__(self):
-        self.bbox_nums = 10
+    def __init__(self,path):
         self.window = tk.Tk()
-        self.sub_var = tk.IntVar()
-        self.pair_sub_var = tk.IntVar() 
-        self.pair_obj_var = tk.IntVar() 
-
-    def start_gui(self,q_json,q_idx):
-        self.window.title("Simple Label")
-        # self.window.geometry('500x200')
+        self.window_right = tk.Frame(self.window)
+        self.window_left = tk.Frame(self.window)
+        self.male_var = tk.IntVar()
+        self.female_var = tk.IntVar()
+        self.child_var = tk.IntVar()
+        self.young_var = tk.IntVar()
+        self.mid_var = tk.IntVar()
+        self.old_var = tk.IntVar()
+        self.bag_var = tk.IntVar()
+        self.mask_var = tk.IntVar()
+        self.hat_var = tk.IntVar()
+        self.luggage_var = tk.IntVar()
+        self.pic_idx_dir = -1
+        self.img = None
+        self.label_img = None
+        self.max_idx = len(os.listdir(path)) 
+        self.img_name = None
+        self.default_save_path = "./MultiLabel.json" 
+        self.to_json = {}
+        self.txt_show = None
+        self.frm = None
+        self.mark_path = "./mark.txt"
         
-        #每个bbox选择一个对象
-        # l = tk.Label(self.window,text="please select a subject for each bbox",bg='green', \
-        #     font=('Arial', 12), width=40, height=1) 
-        # l.pack()
-        # for sub_key,sub_value in subject_cls.items():        
-        #     r1 = tk.Radiobutton(self.window, text=str(sub_value)+":"+sub_key, variable=self.sub_var, \
-        #         value=sub_value, command=None)
-        #     r1.pack()
-        # b = tk.Button(self.window, text='certain', font=('Arial', 10), width=10, height=1, command=lambda:self.annotation(q_json,q_idx))
-        # b.pack()
 
-        #选择为<人-物>
-        frm2_3 = tk.Frame(self.window)
-        frm2 = tk.Frame(frm2_3)
-        l = tk.Label(self.window,text="please select a <person,object> pair",bg='green', font=('Arial', 12), width=40, height=1) 
+        self.varis = [self.male_var,self.female_var,self.child_var,self.young_var,self.mid_var,\
+                self.old_var,self.bag_var,self.mask_var,self.hat_var,self.luggage_var]
+    
+    def win_right_GUI(self):
+        l = tk.Label(self.window_right,text="please select labels for each picture",bg='green', \
+            font=('Arial', 12), width=30, height=1) 
         l.pack()
-        #l_pr = tk.label()
-        for i in range(self.bbox_nums):
-            c2 = tk.Radiobutton(frm2, text = "bbox "+str(i), variable = self.pair_sub_var,value=i, command=None)
-            c2.pack()
-        frm2.pack(side=tk.LEFT)
+        
+        leng    = [len(i) for i in labels]
+        max_len = max(leng)
 
-        frm3 = tk.Frame(frm2_3)
-        for i in range(self.bbox_nums):
-            c2 = tk.Radiobutton(frm3, text = "bbox "+str(i), variable = self.pair_obj_var,value=i, command=None)
-            c2.pack()
-        frm3.pack(side=tk.LEFT)
-        frm2_3.pack()
+        nll = ''
+        for _ in labels:
+            nll += ' ' 
 
-        #选择的<人-物>关系
-        frm4 = tk.Frame(self.window)
+        self.frm = tk.Frame(self.window_right)
 
+        frm1 = tk.Frame(self.frm) 
+        for v,i in zip(self.varis,labels):
+            frm2 = tk.Frame(frm1)
+            s = nll[:max_len-len(i)]+i #右对齐
+            r = tk.Label(frm2,text=s)
+            r.pack(side=tk.LEFT)
+            r1   = tk.Radiobutton(frm2,text='1',variable=v,value=1,command=None)
+            r0   = tk.Radiobutton(frm2,text='0',variable=v,value=0,command=None)
+            r2   = tk.Radiobutton(frm2,text='NotSure',variable=v,value=2,command=None)
+            r1.pack(side=tk.LEFT)
+            r0.pack(side=tk.LEFT)
+            r2.pack(side=tk.LEFT)
+            frm2.pack()
+        frm1.pack(side=tk.LEFT)
+        self.frm.pack()
 
-
-        b = tk.Button(self.window, text='certain', font=('Arial', 10), width=10, height=1, command=lambda:self.hoi_annotation(q_json,q_idx))
+        b = tk.Button(self.window_right, text='certain', font=('Arial', 10), width=10, height=1, command=self.selection)
         b.pack()
 
+        frm2 = tk.Frame(self.window_right)
+        b = tk.Button(frm2, text='pre', font=('Arial', 10), width=10, height=1, command=lambda:self.show_pic(-1))
+        b.pack(side=tk.LEFT)
+        b = tk.Button(frm2, text='next', font=('Arial', 10), width=10, height=1, command=lambda:self.show_pic(1))
+        b.pack(side=tk.LEFT)
+        frm2.pack()
+
+        c = tk.Button(self.window_right, text='to csv', font=('Arial', 10), width=10, height=1, command=self.save_csv)
+        c.pack(pady=20)
+
+        self.window_right.pack(side=tk.RIGHT)
+ 
+ 
+    def start_gui(self):
+        self.window.title("Simple Label(MultiLabel)")
+        self.win_right_GUI()
+        
+        self.txt_show = tk.Text(self.window_left,height=4,width=50)
+        self.txt_show.pack(side=tk.BOTTOM)
+        txt_title = tk.Label(self.window_left,text="Your seclected labels shown in the following box")
+        txt_title.pack(side=tk.BOTTOM)
+
+        self.init_idx()        
+        self.show_pic()
         self.window.mainloop()
 
-    def hoi_annotation(self,q_json,q_idx):
-        pair_sub_var = self.pair_sub_var.get()
-        pair_obj_var = self.pair_obj_var.get()
+    def selection(self):
+        new_dict = {}
 
-        cur_dict = q_json.get()
-      
-        hoi_dict = {"subject_id":pair_sub_var,"object_id":pair_obj_var}
+        for flag,value in zip(labels,self.varis):
+            new_dict[flag] = value.get()
+        # if img information exists in json,update them or add the information.
+        if self.img_name in self.to_json:
+            self.to_json[self.img_name].update(new_dict) 
+        else:
+            self.to_json[self.img_name] = new_dict
+        
+        print(self.to_json)
+        self.show_txt()
+        self.save_json()
+
+    def messagebox_first(self):
+        messagebox.showinfo(title='Warning', message="It's already the first picture!")
+
+    def messagebox_last(self):
+        messagebox.showinfo(title='Warning', message="It's already the last picture!")
+
+    def messagebox_conver(self):
+        messagebox.showinfo(title='ok', message="conver to csv file successfully!")
 
 
-        id_in_json= q_idx.get()
-        q_idx.put(id_in_json)
+    def save_json(self):
+        with open(self.default_save_path,'w') as f:
+            json.dump(self.to_json,f)
 
+        with open(self.mark_path,"w") as f:
+            f.write(self.img_name)
     
+    def img_process(self):
+        height,width,_ = np.shape(self.img)
+        max_l = max(height,width)
+        
+        if max_l> 500 and width>height:
+            ft = 500/max_l
+            self.img = self.img.resize((int(width*ft),int(height*ft)))
+        
+        if max_l>400 and height>width:
+            ft = 400/max_l
+            self.img = self.img.resize((int(width*ft),int(height*ft)))
+    
+    def init_idx(self):
+        if os.path.exists(self.default_save_path) and os.path.exists(self.mark_path):
+            with open(self.mark_path) as f:
+                marked = f.readline()
+                print("mark",marked)
 
-
-        if hoi_dict not in cur_dict[id_in_json]["hoi_annotations"]: #避免重复添加hoi_annotation信息
+            for idx,i in enumerate(os.listdir(img_path)):
+                if i !=marked:
+                    continue
+                else:self.pic_idx_dir = idx-1
 
         
-                cur_dict[id_in_json]["hoi_annotations"].append(hoi_dict)
-                try:
-                    vec = np.array(cur_dict[id_in_json]['annotations'][pair_obj_var]['center'])- \
-                        np.array(cur_dict[id_in_json]['annotations'][pair_sub_var]['center'])
+    def show_txt(self):
+        if self.img_name in self.to_json:
+            txt_to_show = [key for key,value in self.to_json[self.img_name].items() if int(value)==1]
+            self.txt_show.delete(1.0, tk.END)
+            self.txt_show.insert(tk.INSERT,txt_to_show)
+        else:
+            self.txt_show.delete(1.0, tk.END)#delete the first char to the last char in the txt box 
+    
+    def save_csv(self):
+        df = pd.DataFrame(self.to_json).T
+        df.to_csv('MultiLabel.csv')
+        self.messagebox_conver()
 
-                    cur_dict[id_in_json]['annotations'][pair_sub_var]['vec'] = list(vec) 
-                    cur_dict[id_in_json]['annotations'][pair_obj_var]['vec'] = list(vec*(-1))
-                except:pass
-                     
+    def show_pic(self,num=1):
 
+        for v in self.varis: v.set(0)
 
-                print("after hoi added:\n",cur_dict[id_in_json])
-            
-                
-      
-
-        q_json.put(cur_dict)
-
-    # def annotation(self,q_json,q_idx):
-    #     category_id = self.sub_var.get()
-    #     cur_dict = q_json.get()
+        if self.pic_idx_dir+num<0:
+            self.messagebox_first()
+            return None 
+        if self.pic_idx_dir+num>=self.max_idx:
+            self.messagebox_last()
+            return None
         
-    #     id_in_json = q_idx.get()
-    #     q_idx.put(id_in_json)
-    #     cur_dict[id_in_json]["annotations"][-1].update({"category_id":category_id})#更新最后那个的anno
-    #     q_json.put(cur_dict)
+        self.pic_idx_dir += num
 
-    #     print("after bbx added:\n",cur_dict[id_in_json])
+        print(self.pic_idx_dir)
+        self.img_name = os.listdir(img_path)[self.pic_idx_dir]
+        if self.img_name[-3:].lower() not in img_format:
+            print(self.img_name)
+            raise Exception("please input a image!")
 
-    def bbx_category_warning(self,Err):
-        messagebox.showinfo(title='Warning', message="bbx below is Error!\n"+str(Err))   
+        pic_path = os.path.join(img_path,self.img_name) 
+        assert os.path.exists(pic_path),"path not found!"
 
+        #show labels in json
+        self.show_txt()
+
+        self.img = Image.open(pic_path)# use PIL to process picture,or only .gif format can be shown.
+        self.img_process()
+        print(pic_path)
+ 
+        self.img = ImageTk.PhotoImage(self.img)
+        if self.label_img:
+            self.label_img.destroy()
+        self.label_img = tk.Label(self.window_left,image=self.img,width=500, height=400,background='grey')
+        self.label_img.pack()
+        self.window_left.pack(side=tk.LEFT)
+        self.window.mainloop()
+
+
+if __name__ == "__main__":
+    gui = GUI(img_path)
+    gui.start_gui()
 
 
--- arg.py
+++ arg.py
@@ -1,15 +1,8 @@
 import argparse
+import tkinter as tk
 parser = argparse.ArgumentParser()
-parser.add_argument("mode", help="train | test")
-mode = parser.parse_args().mode
 
-img_format = ['png','jpg']
-subject_cls = {'person':1,'face':7} 
-if mode=="train":
-    Hoi_cls ={'with':1,'without':0}
-else:
-    Hoi_cls = {'with':0,'without':1}
+img_format = ['png','jpg','bmp']
+labels = ['male','female','child','young','mid','old','bag','mask','hat','luggage']
 
-import_json = "./label_train.json"
-# import_pic_path = 'Z:/02_dataset/qiuweiyu/to_fgm/csbank/'
-import_pic_path = 'Z:\\gwf\\dataset\\wf'
+img_path = 'C:/Users/Administrator/Desktop/tooltest'
